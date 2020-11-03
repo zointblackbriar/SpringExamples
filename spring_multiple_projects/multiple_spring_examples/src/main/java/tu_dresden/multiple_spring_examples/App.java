599e5bf89607676b0ab9f57fdd3ea99ea7e58d9c
Orcun <orcun.oruc@tu-dresden.de>
2020-11-03 16:56:03 +0100

    multiple_spring_examples activemq

---
+++ spring_multiple_projects/multiple_spring_examples/src/main/java/tu_dresden/multiple_spring_examples/App.java
@@ -0,0 +1,13 @@
+package tu_dresden.multiple_spring_examples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
